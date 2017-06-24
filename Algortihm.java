6c60446fe3a47b57545dd7ed7d65892575179b56
David Espejo Antiñolo <davidespejoantinolo@gmail.com>
2017-06-24 15:33:22 +0200

    Added brackets and fixed errors
    I fixed the error that you couldn't resolve operations with multiple digits and i added brackets.

--- Algortihm.java
+++ Algortihm.java
@@ -9,7 +9,12 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		Fix f=new Fix(s.nextLine());
+		double TInicio, TFin, tiempo;
+		TInicio = System.nanoTime(); 
 		System.out.println(f.toResult(f.toPostFix()));
+		TFin = System.nanoTime();
+		tiempo = TFin - TInicio; 
+		System.out.println("Tiempo de ejecución en milisegundos: " + tiempo/1000000); 
 	}
 
 }
@@ -21,6 +26,7 @@ class Fix{
 	public Vector<String> toPostFix(){
 		Vector<String> postFix=new Vector<String>();
 		Stack<Character> operators=new Stack<Character>();
+		Stack<String> tDig=new Stack<String>();
 		for(int i=0;i<input.length();i++){
 			char elm=input.charAt(i);
 			if(checkOp(elm)){
@@ -34,28 +40,71 @@ class Fix{
 						}
 						operators.push(elm);
 					}else if(p==0){
+						postFix.add(""+operators.pop());
 						operators.push(elm);
+					}
+					if(elm=='(')operators.push(elm);
+					if((char)operators.peek()=='('){
+						operators.push(elm);
+					}
+					if(elm==')'){
+						while(operators.peek()!='('){
+							postFix.add(""+operators.pop());
+						}
 					}
 				}else{
 					operators.push(elm);
 				}
 			}else{
+				/*tDig.add(""+elm);
+				while(!postFix.isEmpty() && !checkOp(postFix.lastElement())){
+					tDig.push(postFix.lastElement());
+					postFix.removeElementAt(postFix.size()-1);
+				}
+				while(!tDig.isEmpty()){
+					postFix.add(tDig.pop());
+					
+				}*/
 				postFix.add(""+elm);
+				if(!checkOp(postFix.lastElement()) && i<input.length()-1 && checkOp(input.charAt(i+1))){
+					//postFix.set(postFix.size()-1,postFix.lastElement()+elm);
+					postFix.add("");
+				}
+				
+				
 			}
 		}
 		 while (!operators.empty()){
 			postFix.add(""+operators.pop());
 		 }
+		 while(postFix.contains("(")){
+			 postFix.remove("(");
+		 }
+		 while(postFix.contains(")")){
+			 postFix.remove(")");
+		 }
+		 int i=-1;
+		 while(i<postFix.size()){
+			 i++;
+			 if(i<postFix.size()-1&& !checkOp(postFix.elementAt(i)) && !checkOp(postFix.elementAt(i+1)) && postFix.elementAt(i+1)!=""){
+				 postFix.set(i, postFix.get(i)+postFix.get(i+1));
+				 postFix.remove(i+1);
+				 i--;
+			 }
+		 }
+		 while(postFix.contains("")){
+			 postFix.remove("");
+		 }
 		return postFix;
 	}
 	public boolean checkOp(char elm){
-		return (elm=='+' || elm=='-' || elm=='*' || elm=='/');
+		return (elm=='+' || elm=='-' || elm=='*' || elm=='/' || elm=='(' || elm==')');
 	}
 	public boolean checkOp(String elm){
-		return (elm.equals("+") || elm.equals("-") || elm.equals("*") || elm.equals("/"));
+		return (elm.equals("+") || elm.equals("-") || elm.equals("*") || elm.equals("/") || elm.equals("(") || elm.equals(")"));
 	}
 	public int priority(char op1,char op2){
-		int res=0;
+		int res=-1;
 		if((op1=='+' || op1=='-')&&(op2=='*' || op2=='/')){
 			res=2;
 		}
@@ -111,4 +160,8 @@ class Fix{
 		float res=Float.parseFloat(str);
 		return res;
 	}	
+	public String toString(float num){
+		String res=Float.toString(num);
+		return res;
+	}
 }
